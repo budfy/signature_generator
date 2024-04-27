8621ed43a270f8adb7d48af7d5189e8fb47fe97f
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2024-04-27 16:21:36 +0300

    Get form data

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -17,7 +17,7 @@ function formSubmit(e) {
             rowspan=4><a href=https://voiceofromni.com.ua
              rel="noopener noreferrer"
              target=_blank><img alt="Voice of Romni logo"
-                 src="https://lh3.googleusercontent.com/drive-viewer/AKGpihZnR_zxBFpoK7Wn-sxoFrdylKqU_ADGgVSLeQTozdKChOgyaiKazuudj9cpcFN8Isj2Gh5G_yR76D6zz-TcVBISOL4nN1pKGAQ=s1600-rw-v1"
+                 src = "https://budfy.github.io/signature_generator/img/Logo round dotted.png"
                  style=width:8em
                  width=100></a>
       <tr>
@@ -35,31 +35,31 @@ function formSubmit(e) {
                    rel="noopener noreferrer"
                    target=_blank
                    style=display:inline-block;width:2em><img alt="Facebook icon"
-                       src="https://lh3.googleusercontent.com/drive-viewer/AKGpihZNmfPNojJjAe0pZz0yoxS8vG77IAcgNvQC39_BwfddlIq1QE_EczG7MEoR_FdmA4OpRDoH3EgcoLckb95g6waUiknPAUM1cag=s1600-rw-v1"
+                       src = "https://budfy.github.io/signature_generator/img/facebook.png"
                        style=width:1.5em></a>
               <td style=padding-right:.5em><a href=https://www.instagram.com/voice_of_romni
                    rel="noopener noreferrer"
                    target=_blank
                    style=display:inline-block;width:1.5em><img alt="Instagram icon"
-                       src="https://lh3.googleusercontent.com/drive-viewer/AKGpihY2y8QDMnxScMjHxTB3ZLk7eq5qbi9ISoXBUhpadJfiFY0BRiSLHiHz7EVEpRUGdjHyQd7N7C31FdGD9Pik3Hkrx9xMbP2ap-4=s1600-rw-v1"
+                       src = "https://budfy.github.io/signature_generator/img/instagram.png"
                        style=width:1.5em></a>
               <td style=padding-right:.5em><a href=https://www.linkedin.com/company/voice-of-romni
                    rel="noopener noreferrer"
                    target=_blank
                    style=display:inline-block;width:1.5em><img alt="Linkedin icon"
-                       src="https://lh3.googleusercontent.com/drive-viewer/AKGpihaVjqMYzD8lySKc6Rm3q9J0fLPB6cBtAawuD_Bh9Fq55zS9J1JPwaZlrVbPi6FncJqMhNw7I1LbC27btzv_cQlg9GyjOd8qsJE=s1600-rw-v1"
+                       src = "https://budfy.github.io/signature_generator/img/linkedin.png"
                        style=width:1.5em></a>
               <td style=padding-right:.5em><a href=https://t.me/serdyuk
                    rel="noopener noreferrer"
                    target=_blank
                    style=display:inline-block;width:1.5em><img alt="Telegram icon"
-                       src="https://lh3.googleusercontent.com/drive-viewer/AKGpihb-iNqG_FTxKx_gtSncIyCOJl9Z2gh1hj9zHhC6vjPCLq5p7lU59Y2AUGUgirTCjsMl1e7BXQ7ZvH01V_sObtFUuGwnrP1uVg=s1600-rw-v1"
+                       src = "https://budfy.github.io/signature_generator/img/telegram.png"
                        style=width:1.5em></a>
               <td style=padding-right:.5em><a href=https://wa.me/+380661143558
                    rel="noopener noreferrer"
                    target=_blank
                    style=display:inline-block;width:1.5em><img alt="Whatsapp icon"
-                       src="https://lh3.googleusercontent.com/drive-viewer/AKGpihYbd4pBImSdqf3pKWdm4gA4TD4YN2xbLYjQRia76SwhkPVi1UmRwiy9lPBtG380uFdfz8yktNC3qEUjKsRQ-pZJo2yipsscVQ=s1600-rw-v1"
+                       src = "https://budfy.github.io/signature_generator/img/whatsapp.png"
                        style=width:1.5em></a>
           </table>
         <td style=padding-left:.5em;padding-right:.5em;padding-top:.5em;padding-bottom:.5em><a href=tel:+3809830706677>+38(098)307 06 77</a>
